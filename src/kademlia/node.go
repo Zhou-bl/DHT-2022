4449859acd095413354723ed2fd123069c670b03
VM_Linux <zhoubinglin2002@163.com>
2022-07-25 02:00:15 -0700

    working on kademlia

--- src/kademlia/node.go
+++ src/kademlia/node.go
@@ -1,7 +1,6 @@
 package kademlia
 
 import (
-	"container/list"
 	"fmt"
 	log "github.com/sirupsen/logrus"
 	"math/big"
@@ -39,12 +38,16 @@ func (this *DataType) data_init() {
 	this.republishTime = make(map[string]time.Time)
 }
 
-type RoutingTable struct {
-	nodeAddr       AddrType
-	rwLock         sync.Mutex
-	buckets        [IDLength]*list.List
-	refreshIndex   int
-	refreshTimeSet [IDLength]time.Time
+type ClosestList struct {
+	Size     int
+	Standard big.Int
+	List     [K]AddrType
+}
+
+type KBucketType struct {
+	size   int
+	bucket [K]AddrType
+	mux    sync.Mutex
 }
 
 type KadNode struct {
@@ -52,7 +55,30 @@ type KadNode struct {
 	data           DataType
 	station        *network
 	conRoutineFlag bool
-	table          RoutingTable
+	routeTable     [M]KBucketType
+	mux            sync.RWMutex
+}
+
+type FindNodeArg struct {
+	TarID  big.Int
+	Sender AddrType
+}
+
+type FindValueArg struct {
+	Key    string
+	Hash   big.Int
+	Sender AddrType
+}
+
+type StoreArg struct {
+	Key    string
+	Value  string
+	Sender AddrType
+}
+
+type FindValueRet struct {
+	First  ClosestList
+	Second string
 }
 
 func (this *KadNode) Init(port int) {
@@ -73,7 +99,22 @@ func (this *KadNode) Run() {
 	}
 }
 
-func (this *KadNode) Join() bool {
+func (this *KadNode) Join(ip string) bool {
+	tmpAddr := AddrType{ip, Hash(ip)}
+	this.kBucketUpdate(tmpAddr)
+	client, tmp_err := Diag(ip)
+	if tmp_err != nil {
+		log.Errorln("[Diag error] in ", ip)
+	} else {
+		var res ClosestList
+		tmp_err = client.Call("WrapperNode.FindNode", &FindNodeArg{this.address.Id, this.address}, &res)
+		for i := 0; i < res.Size; i++ {
+			this.kBucketUpdate(res.List[i])
+		}
+		client.Close()
+	}
+
+	closestlist := this.NodeLookup(&this.address.Id)
 
 }
 
@@ -83,11 +124,82 @@ func (this *KadNode) Ping(addr string) bool {
 }
 
 func (this *KadNode) Put(key string, value string) bool {
+	ketID := Hash(key)
 
 }
 
 func (this *KadNode) Get(key string) (bool, string) {
 
+}
+
+func (this *KadNode) FindNode(tarID *big.Int) (closestList ClosestList) {
+	this.mux.RLock()
+	defer this.mux.RUnlock()
+	closestList.Standard = *tarID
+	for i := 0; i < M; i++ {
+		for j := 0; j < this.routeTable[i].size; j++ {
+			if Ping(this.routeTable[i].bucket[j].Ip) == nil { // if online
+				closestList.Insert(this.routeTable[i].bucket[j])
+			}
+		}
+	}
+	return
+}
+
+func (this *KadNode) FindValue(key string, hash *big.Int) FindValueRet {
+	//firstly find in node "this" then find it in other nodes
+	this.mux.RLock()
+	defer this.mux.RUnlock()
+	founded, value := this.data.GetValue(key)
+	if founded {
+		return FindValueRet{ClosestList{}, value}
+	}
+	retClosest := ClosestList{Standard: *hash}
+	for i := 0; i < M; i++ {
+		for j := 0; j < this.routeTable[i].size; j++ {
+			if Ping(this.routeTable[i].bucket[j].Ip) == nil { //if online
+				retClosest.Insert(this.routeTable[i].bucket[j])
+			}
+		}
+	}
+	return FindValueRet{retClosest, ""}
+}
+
+func (this *KadNode) NodeLookup(tarID *big.Int) (closestList ClosestList) {
+	closestList = this.FindNode(tarID)
+	closestList.Insert(this.address)
+	isUpdate := true
+	diaged := make(map[string]bool)
+	for isUpdate {
+		isUpdate = false
+		var tmp ClosestList
+		var removeList []AddrType
+		for i := 0; i < closestList.Size; i++ {
+			if diaged[closestList.List[i].Ip] == true {
+				continue
+			}
+			this.kBucketUpdate(closestList.List[i])
+			client, tmp_err := Diag(closestList.List[i].Ip)
+			diaged[closestList.List[i].Ip] = true
+			var res ClosestList
+			if tmp_err != nil {
+				removeList = append(removeList, closestList.List[i])
+			} else {
+				tmp_err = client.Call("WrapperNode.FindNode", &FindNodeArg{TarID: *tarID, Sender: this.address}, &res)
+				for j := 0; j < res.Size; j++ {
+					tmp.Insert(res.List[j])
+				}
+				client.Close()
+			}
+		}
+		for _, key := range removeList {
+			closestList.Remove(key)
+		}
+		for i := 0; i < tmp.Size; i++ {
+			isUpdate = isUpdate || closestList.Insert(tmp.List[i])
+		}
+	}
+	return
 }
 
 //private functions:
@@ -96,18 +208,13 @@ func (this *KadNode) reset() {
 	this.data.data_init()
 }
 
-func (this *KadNode) bgMaintain() {
-	go func() {
-		for this.conRoutineFlag {
-
-		}
-	}()
-
-	go func() {
-		for this.conRoutineFlag {
-
-		}
-	}()
+func (this *KadNode) kBucketUpdate(addr AddrType) {
+	this.mux.Lock()
+	defer this.mux.Unlock()
+	if addr.Ip == "" || addr.Ip == this.address.Ip {
+		return
+	}
+	this.routeTable[cpl(&this.address.Id, &addr.Id)].Update(addr)
 }
 
 //none used function
--- src/kademlia/toolfuction.go
+++ src/kademlia/toolfuction.go
@@ -2,19 +2,23 @@ package kademlia
 
 import (
 	"crypto/sha1"
+	"errors"
 	"math/big"
 	"net"
+	"net/rpc"
 	"time"
 )
 
 const M int = 16
 const K int = 10
 const Alpha int = 3
-const IDLength int = 160
-const RemoteTryInterval = 25 * time.Millisecond
-const RePublishInterval = 100 * time.Millisecond
-const UpdateInterval = 25 * time.Millisecond
+const ExpiredTime = 900 * time.Second
+const NeedRepublicTime = 120 * time.Second
 const waitTime = 250 * time.Millisecond
+const UpdateInterval = 25 * time.Millisecond
+const RepublishINterval = 100 * time.Millisecond
+const RemoteTryTime = 3
+const RemoteTryInterval = 250 * time.Millisecond
 
 var Mod = big.NewInt(0).Exp(big.NewInt(2), big.NewInt(int64(M)), nil)
 var localAddress string
@@ -64,4 +68,219 @@ func Hash(str string) big.Int {
 	ret.SetBytes(hasher.Sum(nil))
 	ret.Mod(&ret, Mod)
 	return ret
+}
+
+//for kBucketTYpe
+func (this *KBucketType) Reflesh() {
+	for i := 0; i < this.size; i++ {
+		if Ping(this.bucket[i].Ip) != nil {
+			for j := i + 1; j < this.size; j++ {
+				this.bucket[j-1] = this.bucket[j]
+			}
+			this.size--
+			return
+		}
+	}
+}
+
+func (this *KBucketType) Update(addr AddrType) {
+	this.mux.Lock()
+	defer this.mux.Unlock()
+	if addr.Ip == "" {
+		return
+	}
+	pos := -1
+	for i := 0; i < this.size; i++ {
+		if this.bucket[i].Ip == addr.Ip {
+			pos = i
+			break
+		}
+	}
+	if pos == -1 {
+		if this.size < K {
+			this.size++
+			this.bucket[this.size-1] = addr
+			return
+		} else {
+			if Ping(this.bucket[0].Ip) == nil {
+				//bucket[0] is online
+				head := this.bucket[0]
+				for i := 1; i < K; i++ {
+					this.bucket[i-1] = this.bucket[i]
+				}
+				this.bucket[K-1] = head
+				return
+			} else {
+				//bucket[0] is offline
+				for i := 1; i < K; i++ {
+					this.bucket[i-1] = this.bucket[i]
+				}
+				this.bucket[K-1] = addr
+				return
+			}
+		}
+	} else {
+		for i := pos + 1; i < this.size; i++ {
+			this.bucket[i-1] = this.bucket[i]
+		}
+		this.bucket[this.size-1] = addr
+	}
+}
+
+//for closetlist:
+func (this *ClosestList) Insert(addr AddrType) bool {
+	res := false
+	if Ping(addr.Ip) != nil {
+		return res
+	}
+	for i := 0; i < this.Size; i++ {
+		if this.List[i].Ip == addr.Ip {
+			return res
+		}
+	}
+	newDis := dis(&this.Standard, &addr.Id)
+	if this.Size < K {
+		res = true
+		for i := 0; i < this.Size; i++ {
+			tmpDis := dis(&this.Standard, &addr.Id)
+			if newDis.Cmp(&tmpDis) < 0 {
+				this.Size++
+				for j := this.Size - 1; j > i; j-- {
+					this.List[j] = this.List[j-1]
+				}
+				this.List[i] = addr
+				return res
+			}
+		}
+		this.Size++
+		this.List[this.Size-1] = addr
+		return res
+	} else {
+		for i := 0; i < K; i++ {
+			tmpDis := dis(&this.Standard, &addr.Id)
+			if newDis.Cmp(&tmpDis) < 0 {
+				res = true
+				for j := K - 1; j > i; j-- {
+					this.List[j] = this.List[j-1]
+				}
+				this.List[i] = addr
+				return res
+			}
+		}
+		return res
+	}
+}
+
+func (this *ClosestList) Remove(addr AddrType) bool {
+	for i := 0; i < this.Size; i++ {
+		if this.List[i].Ip == addr.Ip {
+			this.Size--
+			for j := i; j < this.Size; j++ {
+				this.List[j] = this.List[j+1]
+			}
+			return true
+		}
+	}
+	return false
+}
+
+func Ping(addr string) error {
+	var ret *rpc.Client
+	var err error
+	if addr == "" {
+		return errors.New("[error] Empty IP addr")
+	}
+	for i := 0; i < RemoteTryTime; i++ {
+		ret, err = rpc.Dial("tcp", addr)
+		if err == nil {
+			ret.Close()
+			return err
+		}
+		time.Sleep(RemoteTryInterval)
+	}
+	return err
+}
+
+func Diag(addr string) (*rpc.Client, error) {
+	var ret *rpc.Client
+	var err error
+	if addr == "" {
+		return nil, errors.New("ERROR: empty IP addr")
+	}
+	for i := 0; i < RemoteTryTime; i++ {
+		ret, err = rpc.Dial("tcp", addr)
+		if err == nil {
+			return ret, err
+		}
+		time.Sleep(RemoteTryInterval)
+	}
+	return nil, err
+}
+
+//for big.Int
+func dis(x, y *big.Int) big.Int {
+	var res big.Int
+	res.Xor(x, y)
+	return res
+}
+func cpl(x, y *big.Int) int {
+	Xdis := dis(x, y)
+	return Xdis.BitLen() - 1
+}
+
+//for dataType
+func (this *DataType) GetRePublishList() (republishList []string) {
+	this.lock.RLock()
+	defer this.lock.RUnlock()
+	for key, tim := range this.republishTime {
+		if time.Now().After(tim) {
+			republishList = append(republishList, key)
+		}
+	}
+	return republishList
+}
+
+func (this *DataType) DeleteExpiredData() {
+	var expiredList []string
+	this.lock.RLock()
+	for key, tim := range this.validTime {
+		if time.Now().After(tim) {
+			expiredList = append(expiredList, key)
+		}
+	}
+	this.lock.RUnlock()
+	this.lock.Lock()
+	for _, key := range expiredList {
+		delete(this.hashMap, key)
+		delete(this.validTime, key)
+		delete(this.republishTime, key)
+	}
+	this.lock.Unlock()
+}
+
+func (this *DataType) AddPair(key string, value string) {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+	this.hashMap[key] = value
+	this.validTime[key] = time.Now().Add(ExpiredTime)
+	this.republishTime[key] = time.Now().Add(NeedRepublicTime)
+}
+
+func (this *DataType) GetValue(key string) (founded bool, res string) {
+	this.lock.RLock()
+	defer this.lock.RUnlock()
+	res, founded = this.hashMap[key]
+	return founded, res
+}
+
+func (this *DataType) SubPair(key string) (founded bool) {
+	this.lock.Lock()
+	this.lock.Unlock()
+	_, founded = this.hashMap[key]
+	if founded {
+		delete(this.hashMap, key)
+		delete(this.republishTime, key)
+		delete(this.validTime, key)
+	}
+	return founded
 }
